fc1334fafaf972a00f03740546d8ced29724c9bc
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-12-08 16:17:40 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter as  Route, Routes, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
